554afed98637774cfec8007900d226c75a79ebca
Aryel Mota Góis <aryel.gois@gmail.com>
2024-02-21 15:47:32 -0300

    Enable floating for DBeaver during its splash screen

--- config.h
+++ config.h
@@ -26,6 +26,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Java",     NULL,       "Dbeaver",  0,            1,           -1 },
 };
 
 /* layout(s) */
