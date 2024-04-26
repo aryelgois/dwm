471f6ad73118a6a55df0e22b8db56af3355c119e
Aryel Mota Góis <aryel.gois@gmail.com>
2024-04-25 21:33:54 -0300

    Change `termcmd` to start tmux on a main session

--- config.h
+++ config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", "new", "-A", "-s", "main", NULL };
 static const char *passmenucmd[] = { "passmenu", "-l", "6", NULL };
 
 static const Key keys[] = {
